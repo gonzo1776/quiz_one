a41ea39a08143ea6d7d8ccc18fee816afdbcb709
gonzo1776 <mgonzo1785@gmail.com>
2019-07-28 21:23:27 +0000

    quiz one complete

---
+++ app/controllers/quiz_one_controller.rb
@@ -0,0 +1,4 @@
+class QuizOneController < ApplicationController
+    def index
+    end
+end
---
+++ test/controllers/quiz_one_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuizOneControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
